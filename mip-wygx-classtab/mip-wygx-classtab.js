09670a4042a5c052daf440bd3ede6b5b5e7a5276
JunIce <east.qrj@gmail.com>
2017-07-25 18:40:21 +0800

    v1.1

--- mip-wygx-classtab/mip-wygx-classtab.js
+++ mip-wygx-classtab/mip-wygx-classtab.js
@@ -1,57 +1,49 @@
 /**
  * @file mip-wygx-classtab 组件
- * @author
+ * @east_qiu@gmail.com
+ * @for  woyaogexing.com  
  */
 
 define(function (require) {
 	var $ = require('zepto');
+
     var customElement = require('customElement').create();
-    var parent = $('mip-wygx-classtab');
-    var children = parent.children();
-
-    var btnContainer = [];
-    var elContainer = [];
-    for(var i = 0; i < children.length; i ++) {
-        var obj = {
-            nodeName : children[i].nodeName,
-            innerText : children[i].innerText,
-            addClass : $(children[i]).attr('add-class'),
-            bindElement : $(children[i]).attr('to-el'),
-        }
-        btnContainer.push(obj);
-    }
-
-    btnContainer.map(function(i){
-        var el = document.querySelector('#' + i.bindElement);
-        if($('#' + i.bindElement).length == 1) {
-
-            elContainer.push({
-                el : el,
-                classStatus : el.classList
-            })
-        }else{
-            console.error('Bind Element not exists!');
-        }
-    })
-
-    console.log(elContainer)
-    function checkElExist(el){
-        if($("#" + el).length > 0) console.info(el);
-        else console.error('Bind Element not exists!');
-    }
-
-
     
-   
-    function elementClick(){
-    	parent.on('click',function(e){
-    		console.log(e.target)
-    	})
+    function Tabs (el) {
+        this._parent = el; // 父节点
+        this.bindToClassname = el.getAttribute('bind-to');
+        this.bindElements = $(this.bindToClassname);
+        this.Childrens = $(el).children(); // 父节点下的子节点
     }
 
-    customElement.prototype.build = function () {
-		elementClick();
+    Tabs.prototype.addEvent = function(){
+        var self = this;
+
+        //储存样式切换记录
+        var bdEle = [];
+
+        //子节点遍历绑定事件
+        this.Childrens.map(function(index, children){
+            $(children).on('click', function(){
+
+                var that = this;
+                // 清空记录方便下次储存
+                self.bindElements.removeClass(bdEle.join(',')); // 移除上次样式
+                bdEle = []; 
+
+                self.bindElements.addClass($(that).attr('toggle-class')); // 添加样式
+                bdEle.push($(that).attr('toggle-class'));
+            });
+        });
     };
+
+    customElement.prototype.build = function(){
+        var ele = this.element;
+
+        //实例化对象
+        new Tabs(ele).addEvent();
+    };
+    
 
     return customElement;
 });
